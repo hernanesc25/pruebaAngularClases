b6339627342246ef480f91b0a79361fa679b51e6
Hernan Escobar <hernanesc25@gmail.com>
2020-04-12 20:46:59 -0300

    configure Heroku build

--- src/server.js
+++ src/server.js
@@ -3,9 +3,9 @@ import { join } from 'path';
 
 const app = express();
 
-app.use(static(__dirname+'/angular-tour-of-heroes'));
+app.use(static(__dirname+'/dist/angular-tour-of-heroes/'));
 app.get('/',function(req,res){
-    res.sendFile(join(__dirname+'/angular-tour-of-heroes/index.html'));
+    res.sendFile(join(__dirname+'dist/angular-tour-of-heroes/index.html'));
 });
 
 app.listen(process.env.PORT || 8080);
