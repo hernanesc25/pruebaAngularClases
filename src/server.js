49580917a4aeeb1389fa976bb53af95aff6bc3f8
Hernan Escobar <hernanesc25@gmail.com>
2020-04-12 20:34:25 -0300

    Segundo commit. Modificaciones para deploy heroku

--- src/server.js
+++ src/server.js
@@ -1,11 +1,11 @@
-const express = require('express');
-const path = require('path');
+import express, { static } from 'express';
+import { join } from 'path';
 
 const app = express();
 
-app.use(express.static(__dirname+'/dist/angular-tour-of-heroes'));
+app.use(static(__dirname+'/angular-tour-of-heroes'));
 app.get('/',function(req,res){
-    res.sendFile(path.join(__dirname+'/dist/angular-tour-of-heroes/index.html'));
+    res.sendFile(join(__dirname+'/angular-tour-of-heroes/index.html'));
 });
 
 app.listen(process.env.PORT || 8080);
