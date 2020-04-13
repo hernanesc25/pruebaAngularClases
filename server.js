e983d07d0d17a4e6748a1179d59f944f708c51f1
Hernan Escobar <hernanesc25@gmail.com>
2020-04-12 21:41:56 -0300

    Primer commit. Modificacionesserver.js para deploy heroku

--- server.js
+++ server.js
@@ -2,9 +2,9 @@ const express = require ('express');
 const app = express ();
 const path = require ('path');
 
-app.use(express.static(__dirname+'/angular-tour-of-heroes/'));
+app.use(express.static(__dirname+'/dist/angular-tour-of-heroes/'));
 app.listen(process.env.PORT || 8080);
 
 app.get('/*',function(req,res){
-    res.sendFile(path.join(__dirname+'/angular-tour-of-heroes/index.html'));
+    res.sendFile(path.join(__dirname+'/dist/angular-tour-of-heroes/index.html'));
 });
